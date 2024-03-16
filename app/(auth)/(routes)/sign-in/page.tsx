eee57156ad496e3cebf669ca23a55962c4ca8f03
avanish7390 <singhavanish7700@gmail.com>
2024-03-16 15:43:44 +0530

    folder & env setup

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+const SignInPage = () => {
+    return ( 
+        <div>
+            This is unprotected page
+        </div>
+     );
+}
+ 
+export default SignInPage;
---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+   <p>This is a protected page</p>
+  )
+  }
