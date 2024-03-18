fbea74a2f04a0183ec14a0805b48623042f1e586
avanish7390 <singhavanish7700@gmail.com>
2024-03-18 16:36:16 +0530

    complete course price form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,12 +1,13 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { LayoutDashboard } from "lucide-react";
+import { BadgeIndianRupee, LayoutDashboard, ListChecks } from "lucide-react";
 import { redirect } from "next/navigation";
+import { CategoryForm } from "./_components/category-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
+import { PriceForm } from "./_components/price-form";
 import { TitleForm } from "./_components/title-form";
-import { CategoryForm } from "./_components/category-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     const { userId } = auth();
@@ -88,6 +89,33 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
                     }))}/>
 
 
+
+                </div>
+                <div className="space=-y-6">
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBadge icon={ListChecks}/>
+                            <h2 className="text-xl">
+                                Course chapters
+                            </h2>
+
+                        </div>
+                        <div>
+                            TODO: Chapters
+                        </div>
+                    </div>
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBadge icon={BadgeIndianRupee}/>
+                            <h2 className="text-xl">
+                                Sell your courses
+                            </h2>
+
+                        </div>
+                        <PriceForm 
+                        initialData={course}
+                        courseId={course.id}/>
+                    </div>
 
                 </div>
                </div>
